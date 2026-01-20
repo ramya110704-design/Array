422c5ca26cf98a5b423b5b5909f1e2e8cafb824d
ramya110704-design <ramya110704@gmail.com>
2026-01-20 18:47:35 +0530

    ArraySum.java file modified

--- ArraySum.java
+++ ArraySum.java
@@ -6,7 +6,7 @@ public class ArraySum {
         int n = sc.nextInt();
         int[] arr = new int[n];
         int sum = 0;
-        System.out.println("Enter Array elements:")
+        System.out.println("Enter Array elements:");
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
             sum += arr[i];
