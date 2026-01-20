6b067b6c69b61ad68c5d92f3bd9aff25126bdd92
ramya110704-design <ramya110704@gmail.com>
2026-01-20 18:42:27 +0530

    ArraySum.java file created

--- ArraySum.java
+++ ArraySum.java
@@ -2,14 +2,16 @@ import java.util.Scanner;
 public class ArraySum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array Size:");
         int n = sc.nextInt();
         int[] arr = new int[n];
         int sum = 0;
+        System.out.println("Enter Array elements:")
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
             sum += arr[i];
         }
-        System.out.println("Sum :"+ sum);
+        System.out.println("Sum of Array element is:"+ sum);
     }
     
 }
